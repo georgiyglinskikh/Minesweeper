1a33fd349feb8e302cea23f11fbae1b9f437d18c
SchwJ <juliachirkova@e1.ru>
2020-07-03 09:41:24 +0500

    Add circle

--- Program.cs
+++ Program.cs
@@ -13,23 +13,31 @@ namespace Minesweeper
             window.SetVerticalSyncEnabled(true);
 
             window.Closed += (a, e) => { window.Close(); };
-            
+
             var rect = new RectangleShape()
             {
                 Size = new Vector2f(10, 10),
                 FillColor = Color.Yellow,
                 Position = new Vector2f(10, 10)
             };
-            
+
+            var circle = new CircleShape()
+            {
+                Radius = 10,
+                Position = new Vector2f(20,20),
+                FillColor = new Color(130,50,80, 200)
+            };
+
             while (window.IsOpen)
             {
                 window.DispatchEvents();
-                
+
                 window.Clear();
-                
+
                 // Отрисовка
                 window.Draw(rect);
-                
+                window.Draw(circle);
+
                 window.Display();
             }
         }
